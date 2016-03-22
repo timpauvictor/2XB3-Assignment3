cfdbe3e0f8c0c619f48756defc12a8f927295af4
Victor Timpau <timpauvictor@gmail.com>
2016-03-22 13:14:52 -0400

    updated with edge classes

--- src/cas2xb3_A3_timpau_vt/NodeTest.java
+++ src/cas2xb3_A3_timpau_vt/NodeTest.java
@@ -8,13 +8,13 @@ public class NodeTest {
 
 	@Test
 	public void test() {
-		Node newYork = new Node("New York");
-		Node philadelphia = new Node("Philadelphia");
-		Node pittsburg = new Node("Pittsburg");
-		newYork.addConnection(philadelphia);
-		newYork.addConnection(pittsburg);
-		
-		pass(newYork.toString().equals("New York is connected to Philadelphia, Pittsburg"));
+		Node newYork = new Node("New York", "NY");
+		Node philadelphia = new Node("Philadelphia", "PA");
+		Node pittsburg = new Node("Pittsburg", "PA");
+		Edge newEdge = new Edge(newYork, pittsburg, 15);
+		assert(newEdge.toString().equals("New York,NY-Pittsburg,PA 15"));
+		newEdge = new Edge(newYork, philadelphia, 12);
+		assert(newEdge.toString().equals("New York,NY-Philadelphia,PA 15"));
 	}
 
 }
