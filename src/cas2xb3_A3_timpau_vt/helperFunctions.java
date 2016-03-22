f3e9066b617fff1734e739181a5bc04402907e22
Victor Timpau <timpauvictor@gmail.com>
2016-03-22 11:08:35 -0400

    added functions to helper

--- src/cas2xb3_A3_timpau_vt/helperFunctions.java
+++ src/cas2xb3_A3_timpau_vt/helperFunctions.java
@@ -1,5 +1,45 @@
 package cas2xb3_A3_timpau_vt;
 
-public class helperFunctions {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Vector;
 
+public class helperFunctions {
+	private fileIO io = new fileIO();
+	
+	private String startCity;
+	private String destCity;
+	private Vector<Node> allNodes = new Vector();
+	
+	public void getInputCities() {
+		Path file = new File("../data/a3_in.txt").toPath();
+		String[] stringArr = io.readFile(file); 
+		setStartCity(stringArr[0]);
+		setDestCity(stringArr[1]);
+	}
+	
+	public String getStartCity() {
+		return startCity;
+	}
+
+
+	public void setStartCity(String startCity) {
+		this.startCity = startCity;
+	}
+
+
+	public String getDestCity() {
+		return destCity;
+	}
+
+
+	public void setDestCity(String destCity) {
+		this.destCity = destCity;
+	}
+
+	public void makeGraph() {
+		Path file = 
+	}
 }
