1dba1c149b8f4b7ff3852b3ef4fe1253399283d0
Victor Timpau <timpauvictor@gmail.com>
2016-03-22 13:15:18 -0400

    added edge class

---
+++ src/cas2xb3_A3_timpau_vt/Edge.java
@@ -0,0 +1,25 @@
+package cas2xb3_A3_timpau_vt;
+
+public class Edge {
+	private Node fromNode;
+	private Node toNode;
+	private int weight;
+	
+	public Edge(Node from, Node to, int weight) {
+		this.fromNode = from;
+		this.toNode = to;
+		this.weight = weight;
+	}
+	
+	public Node getFromNode() {
+		return this.fromNode;
+	}
+	
+	public Node getToNode() {
+		return this.toNode;
+	}
+	
+	public String toString() {
+		return fromNode + "-" + toNode + " " + weight;
+	}
+}
