ce65defe31b57fcc1157d078daca7903e5922188
Victor Timpau <timpauvictor@gmail.com>
2016-03-22 11:08:06 -0400

    created fileReader function in fileIO class

--- src/cas2xb3_A3_timpau_vt/fileIO.java
+++ src/cas2xb3_A3_timpau_vt/fileIO.java
@@ -1,5 +1,17 @@
 package cas2xb3_A3_timpau_vt;
 
-public class fileIO {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public class fileIO {
+	public String[] readFile(Path filePath) {
+		String[] stringArr = null;
+		try { 
+			stringArr = (String[]) Files.readAllLines(filePath).toArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return stringArr;
+	}
 }
